a27a86a8fd558b9d4b19f22d5eac5d48d8979876
abdulaziz7549 <abdumannopov688@gmail.com>
2023-07-28 12:19:48 +0500

    js code

---
+++ app.js
@@ -0,0 +1,18 @@
+// let . var . const 
+// console.log . console.group . console.table . console.assert
+// var bu ozgaruvchi bolib variable digan manoni anglatadi
+
+
+
+
+// var num = 30;
+// var num1 = 40 
+// alert(num+num1)
+
+
+var a = prompt("1 qiymatni kirit")
+var b = prompt("2 qiymatni kirit") 
+
+c = a**2 + b**2 
+ 
+console.log(Math.sqrt(c))
